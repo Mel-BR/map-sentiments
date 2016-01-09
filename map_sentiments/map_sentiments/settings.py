72d331fd5e300c4106ea5d60dd65479f6c6d5951
Melissa BR <melissa.benali-richard@etu.utc.fr>
2016-01-09 11:04:26 +0100

    Add lines for Static and Templates Dir in the settings

--- map_sentiments/map_sentiments/settings.py
+++ map_sentiments/map_sentiments/settings.py
@@ -16,6 +16,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -101,3 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
